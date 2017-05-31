8995e783f6f7309e8577ef6e4232b57e89d9286b
MrManz <vincentmanz@aol.com>
2017-05-31 12:21:56 +0200

    added Google login

--- Backend/Scheduler.go
+++ Backend/Scheduler.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/contrib/sessions"
 )
 
 var concreteBarMachine = BarMachine{Id:"01"}
@@ -12,6 +14,14 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	router := gin.Default()
+	router.Use(sessions.Sessions("goquestsession", store))
+
+	router.GET("/login", loginHandler)
+	router.GET("/auth", authHandler)
+
+	router.Run("127.0.0.1:9090")
 
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
