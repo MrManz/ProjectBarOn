310574c442775a8bbe7dfb42483c6334061e7231
ThraexDev <peter.truckenbrod@sap.com>
2017-06-11 23:35:59 +0200

    database connection factory + mock erstellt

--- Backend/main/Scheduler.go
+++ Backend/main/Scheduler.go
@@ -8,6 +8,11 @@ func main() {
 	//Testrequest: GET localhost:8080/beispiel
 	//Header: Authorization=HIER TOKEN EINFÜGEN
 	http.Handle("/beispiel", authMiddleware(CreateBeispielHandler()))
-	http.Handle("/order", authMiddleware(CreateOrderHandler()))
+	db_factory :=Create_db_connector_factory()
+	db_con:=db_factory.make("mock")
+	orderHander:=CreateOrderHandler(db_con)
+	http.Handle("/order", authMiddleware(orderHander))
+	amountHandler:=CreateGetAmountHandler(db_con)
+	http.Handle("/getamount", authMiddleware(amountHandler))
 	http.ListenAndServe(":8080", nil)
 }
