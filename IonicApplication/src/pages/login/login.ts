b8996893f0a151c673e4037e9a4a6f42e2820957
wolfmarcel <marcel@wolf4.de>
2017-06-20 12:23:10 +0200

    Auth fix

--- IonicApplication/src/pages/login/login.ts
+++ IonicApplication/src/pages/login/login.ts
@@ -3,6 +3,7 @@ import {NavController, LoadingController, Platform} from 'ionic-angular';
 //import {BenutzerkontoPage} from '../benutzerkonto/benutzerkonto';
 import {GooglePlus, NativeStorage} from 'ionic-native';
 import {TabsControllerPage} from '../tabs-controller/tabs-controller';
+import GoogleAuth = gapi.auth2.GoogleAuth;
 var that: LoginPage;
 @Component({
   selector: 'page-login',
@@ -12,6 +13,7 @@ export class LoginPage {
   isApp: boolean;
   SCOPE = 'profile';
   GoogleAuth: any;
+  loading
   // this tells the tabs component which Pages
   // should be each tab's root Page
   constructor(public navCtrl: NavController, public loadingCtrl: LoadingController, public platform: Platform) {
@@ -20,23 +22,25 @@ export class LoginPage {
     } else {
       this.isApp = true;
     }
+    this.loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
     that = this;
   }
+
   // Do Google Login depending on mobile or Desktop
   goToBenutzerkonto(params) {
+
     if (this.isApp) {
       let nav = this.navCtrl;
-      let loading = this.loadingCtrl.create({
-        content: 'Please wait...'
-      });
-      loading.present();
+      this.loading.present();
       GooglePlus.login({
         'scopes': '', // optional, space-separated list of scopes, If not included or empty, defaults to `profile` and `email`.
         'webClientId': '920043719912-b9pam9s4ak2g3f3elv421hk5d2jqhmjg.apps.googleusercontent.com', // optional clientId of your Web application from Credentials settings of your project - On Android, this MUST be included to get an idToken. On iOS, it is not required.
         'offline': true
       })
         .then(function (user) {
-          loading.dismiss();
+          this.loading.dismiss();
 
           NativeStorage.setItem('user', {
             name: user.displayName,
@@ -49,14 +53,12 @@ export class LoginPage {
               console.log(error);
             })
         }, function (error) {
-          loading.dismiss();
+          this.loading.dismiss();
         });
     } else {
+      this.loading.present();
       gapi.load('client:auth2', this.initClient);
     }
-
-    if (!params) params = {};
-    this.navCtrl.push(TabsControllerPage);
     //this.navCtrl.push(BenutzerkontoPage);
   }
 
@@ -67,9 +69,13 @@ export class LoginPage {
       'scope': that.SCOPE
     }).then(function () {
       that.GoogleAuth = gapi.auth2.getAuthInstance();
-      var user = that.GoogleAuth.currentUser.get();
-      //that.setSigninStatus();
-      that.GoogleAuth.signIn();
+      that.GoogleAuth.signIn().then(function () {
+        var user = that.GoogleAuth.currentUser.get();
+        console.log(user.getBasicProfile().getEmail());
+        that.navCtrl.push(TabsControllerPage);
+        that.loading.dismiss();
+      });
+      //if (!params) params = {};
     });
   }
 
