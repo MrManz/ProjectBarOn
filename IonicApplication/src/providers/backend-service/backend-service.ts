d5a468267bcd38c9cacfe16c1fe901abbb4ffb78
ANEDev <airbagex@gmail.com>
2017-08-14 14:41:53 +0200

    Bestellübersicht hinzugefügt

--- IonicApplication/src/providers/backend-service/backend-service.ts
+++ IonicApplication/src/providers/backend-service/backend-service.ts
@@ -3,12 +3,15 @@ import 'rxjs/add/operator/map';
 import { Http, Headers, RequestOptions} from '@angular/http'
 
 /*
-  ServiceProvider-Class zur Interaktion mit den Backend-APIs
+  Generated class for the BackendServiceProvider provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
 */
 @Injectable()
 export class BackendServiceProvider {
   data;
-  host = "http://localhost:8080" //"http://10.0.2.2:8080"
+  host = "http://localhost:8080" // "http://10.0.2.2:8080"
 
   constructor(private http: Http) {
   }
@@ -67,6 +70,22 @@ export class BackendServiceProvider {
 
     });
   }
+
+  loadOrderedDrinks(userToken) {
+    return new Promise((resolve, reject) => {
+      var headers = new Headers();
+      headers.append("Authorization", userToken);
+      let options = new RequestOptions({ headers: headers });
+
+      this.http.post(this.host+'/getamount', options)
+        .subscribe(data => {
+          resolve(data);
+        }, error => {
+          reject(error);
+        });
+    });
+  }
+
   likeRecipe(RecipeID, userToken){
     return new Promise((resolve, reject) => {
       var headers = new Headers();
